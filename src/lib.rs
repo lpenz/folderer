4da5f1190f3647eab362176bec9fa680640afc2b
Leandro Lisboa Penz <lpenz@lpenz.org>
2022-04-10 19:05:08 +0100

    Initial code, with CI

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright (C) 2022 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+// #![warn(missing_docs)]
+// #![deny(rustdoc::broken_intra_doc_links)]
+
+mod accumulator;
+pub use self::accumulator::*;
